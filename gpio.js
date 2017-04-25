3fe9f7c0e4e6d91e2c47c3d8c7790c1a5c65e961
Pol Ruzafa <kiari62@gmail.com>
2017-04-25 23:19:05 +0200

    fix issue introduced by last commit

--- gpio.js
+++ gpio.js
@@ -9,7 +9,7 @@ rpio.init();
 function reproduce(message){
     
 
-    console.log(message);
+    //console.log(message);
 
     /*for(let i = 0; i < 5; i++){
         rpio.write(11, rpio.HIGH);
@@ -20,14 +20,21 @@ function reproduce(message){
     }*/
     let morse = "";
     for(let word in message){
-        for(letter in word){
-            console.log(letter);
-        }
+        console.log(word);
     }
 
     rpio.close(11);
 }
+// Make LED blink - dash
 function dash(){
+    rpio.open(11, rpio.OUTPUT, rpio.LOW);
+
+
+
+    rpio.close(11);
+}
+// Make LED blink - dot
+function dot(){
     rpio.open(11, rpio.OUTPUT, rpio.LOW);
 
 
