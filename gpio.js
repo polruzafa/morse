e8c07650d0b9fc682a3f29313ce09c0f91f4518f
Pol Ruzafa <kiari62@gmail.com>
2017-04-25 23:27:51 +0200

    fix issue introduced by last commit

--- gpio.js
+++ gpio.js
@@ -1,35 +1,40 @@
 var rpio = require("rpio");
 
-const SHORT = 200;
-const LONG = 600;
+const UNIT = 200;
+const SHORT = UNIT;
+const LONG = UNIT * 3;
+const INTER_ELEMENT = UNIT;
+const INTER_LETTER = UNIT * 3;
+const INTER_WORD = UNIT * 7;
 
 // Init GPIO
 rpio.init();
 
 function reproduce(message){
-    
-
-    //console.log(message);
-
-    /*for(let i = 0; i < 5; i++){
-        rpio.write(11, rpio.HIGH);
-        rpio.sleep(1);
-
-        rpio.write(11, rpio.LOW);
-        rpio.msleep(500);
-    }*/
     let morse = "";
     for(let word in message){
-        console.log(message[word]);
+        //console.log(message[word]);
+        for(let letter in word){
+            for(let sign in letter){
+                if(letter[sign] == "."){
+                    dot();
+                }else{
+                    dash();
+                }
+                rpio.msleep(INTER_ELEMENT);
+            }
+            rpio.msleep(INTER_LETTER);
+        }
+        rpio.msleep(INTER_WORD);
     }
-
-    rpio.close(11);
 }
 // Make LED blink - dash
 function dash(){
     rpio.open(11, rpio.OUTPUT, rpio.LOW);
 
-
+    rpio.write(11, rpio.HIGH);
+    rpio.msleep(LONG);
+    rpio.write(11, rpio.LOW);
 
     rpio.close(11);
 }
@@ -37,7 +42,9 @@ function dash(){
 function dot(){
     rpio.open(11, rpio.OUTPUT, rpio.LOW);
 
-
+    rpio.write(11, rpio.HIGH);
+    rpio.msleep(SHORT);
+    rpio.write(11, rpio.LOW);
 
     rpio.close(11);
 }
